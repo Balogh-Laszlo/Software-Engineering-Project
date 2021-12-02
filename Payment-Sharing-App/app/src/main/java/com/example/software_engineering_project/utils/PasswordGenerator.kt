4037f684c66dce5fa7269fdddf4f6019424246be
Balogh-Laszlo <79311989+Balogh-Laszlo@users.noreply.github.com>
2021-12-02 15:41:24 +0200

    feat: Database plan

---
+++ Payment-Sharing-App/app/src/main/java/com/example/software_engineering_project/utils/PasswordGenerator.kt
@@ -0,0 +1,38 @@
+package com.example.software_engineering_project.utils
+
+import java.security.SecureRandom
+//source https://medium.com/android-news/password-generator-and-tester-with-kotlin-6db3c22488ff
+class PasswordGenerator {
+    val letters : String = "abcdefghijklmnopqrstuvwxyz"
+    val uppercaseLetters : String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val numbers : String = "0123456789"
+    val special : String = "@#=+!£$%&?"
+    val maxPasswordLength : Float = 20F
+    val maxPasswordFactor : Float = 10F
+
+    fun generatePassword(isWithLetters: Boolean,
+                         isWithUppercase: Boolean,
+                         isWithNumbers: Boolean,
+                         isWithSpecial: Boolean,
+                         length: Int) : String {
+
+        var result = ""
+        var i = 0
+
+        if(isWithLetters){ result += this.letters }
+        if(isWithUppercase){ result += this.uppercaseLetters }
+        if(isWithNumbers){ result += this.numbers }
+        if(isWithSpecial){ result += this.special }
+
+        val rnd = SecureRandom.getInstance("SHA1PRNG")
+        val sb = StringBuilder(length)
+
+        while (i < length) {
+            val randomInt : Int = rnd.nextInt(result.length)
+            sb.append(result[randomInt])
+            i++
+        }
+
+        return sb.toString()
+    }
+}
